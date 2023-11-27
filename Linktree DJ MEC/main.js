b6711c87813ca69aafdddd120cef3e0b5fbd4935
felipesilvy <139032099+felipesilvy@users.noreply.github.com>
2023-11-26 21:16:32 -0300

    Add files via upload

---
+++ Linktree DJ MEC/main.js
@@ -0,0 +1,70 @@
+window.onload = function () {
+  let botao = document.getElementsByClassName("btn btn-outline-dark");
+  console.log(botao);
+  botao[0].onclick = function () {
+    window.open(
+      "https://tidal.com/browse/playlist/d044f4b2-ca9e-4177-8ed0-48aa456ee82b"
+    );
+  };
+  botao[1].onclick = function () {
+    window.open(
+      "https://tidal.com/browse/playlist/4ca2247d-d8d0-49ff-b43b-8aa33081bed9"
+    );
+  };
+  botao[2].onclick = function () {
+    window.open(
+      "https://tidal.com/browse/playlist/49f217f6-dd13-415b-b7aa-c09cbdf75a23"
+    );
+  };
+  botao[3].onclick = function () {
+    window.open(
+      "https://music.amazon.com.br/user-playlists/0015f637324d4a12b18d0ad9d7b93d24b1r0?ref=dm_sh_JF2NovMyOha0XrH9QdEbeGHzl"
+    );
+  };
+  botao[4].onclick = function () {
+    window.open("https://www.djmec.com.br/agenda.html");
+  };
+  botao[5].onclick = function () {
+    window.open("https://djmecflashback.minharadioonline.net/");
+  };
+  botao[6].onclick = function () {
+    window.open(
+      "https://play.google.com/store/apps/details?id=br.com.radiosapp5.webradiodjmec&pli=1"
+    );
+  };
+  botao[7].onclick = function () {
+    window.open("https://www.radios.com.br/aovivo/web-radio-dj-mec/139063");
+  };
+  botao[8].onclick = function () {
+    window.open("https://www.cxradio.com.br/radio/web-radio-dj-mec-itajai");
+  };
+  botao[9].onclick = function () {
+    window.open("https://radio.garden/listen/web-radio-dj-mec/yPYLUjYr");
+  };
+  botao[10].onclick = function () {
+    window.open("https://www.instagram.com/djmecflashback/");
+  };
+  botao[11].onclick = function () {
+    window.open("https://www.djmec.com.br/festa_do_ano.html");
+  };
+  botao[12].onclick = function () {
+    window.open(
+      "https://soundcloud.com/user-714492164/sets/pop-rock-anos-70-80-90"
+    );
+  };
+  botao[13].onclick = function () {
+    window.open("https://www.radiozero80.com.br/");
+  };
+  botao[14].onclick = function () {
+    window.open("https://www.facebook.com/djmecflashback");
+  };
+  botao[15].onclick = function () {
+    window.open("https://www.facebook.com/musicaflashbackretro");
+  };
+  botao[16].onclick = function () {
+    window.open("hhttps://www.djmec.com.br/webradiodjmec.html");
+  };
+  botao[17].onclick = function () {
+    window.open("https://www.djmec.com.br/");
+  };
+};
